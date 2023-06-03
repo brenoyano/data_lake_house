344871d0ee1d57db5dde936f473857e7ede5bf1f
brenoyano <brenoyano@gmail.com>
2023-06-03 13:51:47 +1000

    Initial commit

---
+++ .docker/dependencies/jupyter_magic.py
@@ -0,0 +1,37 @@
+import pyspark
+from delta import *
+from IPython.core.magic import (register_line_magic, register_cell_magic,
+register_line_cell_magic)
+import ipywidgets as w
+
+
+#Magics
+
+@register_cell_magic
+def sql(cell,query=None):
+    from datetime import datetime
+
+    if not query.lower().startswith("select"):
+        spark.sql(str(query))
+    else:
+        now = datetime.now()
+        now = now.strftime("%Y%m%d%H%M%S")
+        toggle = w.ToggleButton(description='Download',
+                            layout=w.Layout(width='100px')
+                            )
+        out = w.Output(layout=w.Layout(border = '1px solid black'))
+        table_query = spark.sql(str(query)).limit(1000).toPandas().head()
+        def fun(obj):
+            with out:
+                spark.sql(str(query)).toPandas().head().to_csv(f'./{now}_data.csv', index=False)
+                out.clear_output()
+        display(table_query)
+        toggle.observe(fun, 'value')
+        display(toggle)
+        display(out)
+
+#Spark
+builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
