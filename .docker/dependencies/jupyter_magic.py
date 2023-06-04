11e26d4135a182789a799a9edb9861976d7ca99e
brenoyano <brenoyano@gmail.com>
2023-06-04 18:48:43 +1000

    Several changes to services communication

--- .docker/dependencies/jupyter_magic.py
+++ .docker/dependencies/jupyter_magic.py
@@ -31,7 +31,7 @@ def sql(cell,query=None):
         display(out)
 
 #Spark
-builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
-    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
-    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-spark = configure_spark_with_delta_pip(builder).getOrCreate()
+#builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
+#    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+#    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+#spark = configure_spark_with_delta_pip(builder).getOrCreate()
